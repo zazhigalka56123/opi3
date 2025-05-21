c6fc74f6852f90ce76870a790ea818fa353975e6
red <red@yandex.ru>
2025-05-22 01:23:23 +0300

    Hello world

---
+++ src/test/java/labs/SampleTest.java
@@ -0,0 +1,11 @@
+package labs;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+    @Test
+    void helloTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
